82070511babf51ccb25ba0305c8fe96b7174886b
Dage Oraca Todico <66525747+BorzXy@users.noreply.github.com>
2026-03-03 21:23:14 +0800

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,67 +1,30 @@
-import axios from "axios";
-
-const wordindexindo = [
-    "https://raw.githubusercontent.com/agulagul/Indonesia-words/refs/heads/master/kata.txt",
-    "https://raw.githubusercontent.com/damzaky/kumpulan-kata-bahasa-indonesia-KBBI/refs/heads/master/list_1.0.0.txt",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/a",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/b",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/c",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/d",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/e",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/f",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/g",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/h",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/i",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/j",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/k",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/l",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/m",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/n",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/o",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/p",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/q",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/r",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/s",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/t",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/u",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/v",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/w",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/x",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/y",
-    "https://raw.githubusercontent.com/bekicot/indonesian_words/refs/heads/master/words/z",
-    "https://raw.githubusercontent.com/BorzXy/wordlist-indo/refs/heads/main/indo",
-    "https://raw.githubusercontent.com/BorzXy/wordlist-indo/refs/heads/main/kbbi_words.txt"
-];
+import fs from "fs";
+import path from "path";
 
 let cachedWords = null;
 
-async function loadWords() {
+function loadWords() {
     if (cachedWords) return cachedWords;
 
-    let allWords = [];
+    const filePath = path.join(process.cwd(), "words.txt");
+    const fileContent = fs.readFileSync(filePath, "utf8");
 
-    for (const url of wordindexindo) {
-        try {
-            const res = await axios.get(url);
-            const words = res.data.split("\n").map(w => w.trim()).filter(Boolean);
-            allWords.push(...words);
-        } catch (err) {
-            console.log("Failed:", url);
-        }
-    }
+    cachedWords = fileContent
+        .split("\n")
+        .map(w => w.trim())
+        .filter(Boolean);
 
-    cachedWords = [...new Set(allWords)];
     return cachedWords;
 }
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
     const { prefix = "" } = req.query;
 
     if (!prefix) {
         return res.status(200).json([]);
     }
 
-    const words = await loadWords();
+    const words = loadWords();
 
     const results = words
         .filter(w => w.toLowerCase().startsWith(prefix.toLowerCase()))
